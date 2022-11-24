ecb27acc79c7f1f95d2ab574d3f5fb9325c5b2c6
oferGavrilov <118836078+oferGavrilov@users.noreply.github.com>
2022-11-24 17:37:36 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -58,6 +58,36 @@ function createMat(ROWS, COLS) {
     return mat
 }
 
+function getEmptyCells(board) {
+    const emptyCells = []
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[i].length; j++) {
+            if (board[i][j].isShown || board[i][j].isMine) continue
+
+            emptyCells.push({ i, j })
+        }
+    }
+    return (shuffle(emptyCells))
+}
+
+
+function emptyCellHint(board) {
+    const emptyCells = []
+    console.log(negsCounter)
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[i].length; j++) {
+            if (board[i][j].isShown) continue
+            var negsCounter = minesNegsCount(i, j)
+            if (negsCounter === 0) {
+                emptyCells.push({ i, j })
+            }
+        }
+    }
+    // console.log(emptyCells)
+    // console.log(drawNum(shuffle( emptyCells)))
+    return (drawNum(shuffle(emptyCells)))
+}
+
 
 
 function renderBoard(board) {
@@ -141,4 +171,31 @@ function onHandleKey(event) {
     }
     // }
 
+}
+
+function showHints() {
+    for (var i = 1; i <= 3; i++) {
+        document.querySelector(`.hint-${i}`).classList.remove('hidden')
+    }
+}
+
+function showElement(selector) {
+    document.querySelector(`${selector}`).classList.remove('hidden')
+}
+function hideElement(selector) {
+    document.querySelector(`${selector}`).classList.add('hidden')
+}
+
+function showNegs(cellI, cellJ) {
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= gBoard.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            // if (i === cellI && j === cellJ) continue
+            if (j < 0 || j >= gBoard[i].length) continue
+            gBoard[i][j].isShown = true
+            renderBoard(gBoard)
+            gNeighbors.push({ i, j })
+        }
+    }
+    console.log(gNeighbors)
 }
